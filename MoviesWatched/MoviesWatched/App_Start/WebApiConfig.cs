c5db436eb9d11a13b6aa10101f5d43c737246cf4
Stephen Panzer <radicaledward@gmail.com>
2018-05-21 15:35:48 -0600

    Change a few serializer settings to work better with Ember Data, include model validation errors for debugging

--- MoviesWatched/MoviesWatched/App_Start/WebApiConfig.cs
+++ MoviesWatched/MoviesWatched/App_Start/WebApiConfig.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using Newtonsoft.Json.Serialization;
 using Saule.Http;
 
 namespace MoviesWatched
@@ -15,8 +20,9 @@ namespace MoviesWatched
             // Web API routes
             config.EnableCors();
             config.MapHttpAttributeRoutes();
-            config.ConfigureJsonApi();
-
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling
+                = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
@@ -24,4 +30,16 @@ namespace MoviesWatched
             );
         }
     }
+
+        public class ValidateModelAttribute : ActionFilterAttribute
+        {
+            public override void OnActionExecuting(HttpActionContext actionContext)
+            {
+                if (actionContext.ModelState.IsValid == false)
+                {
+                    actionContext.Response = actionContext.Request.CreateErrorResponse(
+                        HttpStatusCode.BadRequest, actionContext.ModelState);
+                }
+            }
+        }
 }
